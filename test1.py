b11ac5acdbaebbf92e3ad1f54819d34561f69656
Roberto Arcomano <info@robertoarcomano.com>
2024-08-30 16:01:18 +1200

    First Commit

--- test1.py
+++ test1.py
@@ -1,9 +1,3 @@
-# spacy
-spaCy userful test
-
-## Test1.py
-### Code
-```
 import spacy
 
 # Load English tokenizer, tagger, parser and NER
@@ -24,18 +18,4 @@ print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 
 # Find named entities, phrases and concepts
 for entity in doc.ents:
-    print(entity.text, entity.label_)```
-```
-### Output
-```
-/home/berto/AquaProjects/src/spacy/venv/bin/python /home/berto/AquaProjects/src/spacy/test1.py
-Noun phrases: ['Sebastian Thrun', 'self-driving cars', 'Google', 'few people', 'the company', 'him', 'I', 'you', 'very senior CEOs', 'major American car companies', 'my hand', 'I', 'Thrun', 'an interview', 'Recode']
-Verbs: ['start', 'work', 'drive', 'take', 'tell', 'shake', 'turn', 'talk', 'say']
-Sebastian Thrun PERSON
-Google ORG
-2007 DATE
-American NORP
-Thrun PERSON
-Recode ORG
-earlier this week DATE
-```
+    print(entity.text, entity.label_)
